34b9796528553c68244080b7faab0c06c9369c11
thomas devasia <tdevasia1997@gmail.com>
2022-12-04 00:21:42 +0000

    new class file error handling

--- main.py
+++ main.py
@@ -1,176 +1,24 @@
-import sys
-import configparser
 import os
-import webbrowser
-import requests
-import base64
-import shutil
+import sys
 import re
+import shutil
 
 from tqdm import tqdm
+
 import PyPDF2
 import pandas as pd
 
-# Microsoft Authentication Library
-import msal
+
+# Microsoft API Class
+from microsoftgraph import microsoftGraph 
 
 # Global Variables
 Download_Cache = './response_downloads/'
 Output = './output/'
 
-# Microsoft Graph API Class
-class microsoftGraph:
 
-    def __init__(self, cfgFile):
-        config = configparser.ConfigParser()
-        config.read([cfgFile])
-        azure_settings = config['azure']
-
-        # client and tenant, id and scopes
-        self.client_id = azure_settings['clientId']
-        self.tenant_id = azure_settings['tenantId']
-        self.scopes = azure_settings['graphUserScopes'].split(' ')
-        
-        self.Graph_API_Endpoint = 'https://graph.microsoft.com/v1.0'
-        
-        self.token = self.get_token()    
-
-    # return token for further api request
-    def get_token(self):
-
-        if os.path.exists('access_token.json'):
-            print('Found access_token.json')
-
-            access_token_cache = msal.SerializableTokenCache()
-
-            with open('access_token.json', 'r') as f:
-                access_token_cache.deserialize(f.read())
-
-            client = msal.PublicClientApplication(self.client_id, token_cache=access_token_cache)
-            
-            
-            accounts = client.get_accounts()[0]
-            print(accounts)
-
-            token = client.acquire_token_silent(self.scopes, account=accounts)
-
-        else:
-            print('No access_token.json found')
-            token = self.create_access_token()        
-        
-        return token
-
-    
-    # creating access token JSON file for the account
-    def create_access_token(self):
-        print("Creating access token")
-
-        access_token_cache = msal.SerializableTokenCache()
-
-        client = msal.PublicClientApplication(self.client_id, token_cache=access_token_cache)
-        flow = client.initiate_device_flow(scopes=self.scopes)
-
-        try:
-            verification_uri = flow['verification_uri']
-            user_code = flow['user_code']
-            print(flow['message'])
-            webbrowser.open(verification_uri)
-
-            # response access token and saving it
-            access_token = client.acquire_token_by_device_flow(flow)
-
-            with open('access_token.json', 'w') as f:
-                f.write(access_token_cache.serialize())
-        
-        except:
-            print('Error in creating access token')
-            print(flow)
-            sys.exit(1)
-        return access_token
-
-
-    # Send request to the Graph API
-    def sendRequest(self, endpoint):
-
-        headers = {
-            'Authorization': 'Bearer ' + self.token['access_token']
-        }
-
-        response = requests.get(endpoint, headers=headers)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print('Error in sending request')
-            print(response.json())
-            sys.exit(1)
-
-    # Search Mail with the search text
-    def searchMail(self, searchText, hasAttachments=False):
-        # print('Searching for correct mails for {}'.format(searchText))
-
-        endpoint = self.Graph_API_Endpoint + f'/me/messages?$search="{searchText}"'
-
-        response = self.sendRequest(endpoint)
-        
-        mails = response['value']
-        print(mails)
-
-        if hasAttachments:
-            temp = mails
-            mails = []
-            for mail in temp:
-                if mail['hasAttachments'] == True:
-                    mails.append(mail)
-        
-        return mails
-
-    def getAttachments(self, mailId, download=False, downloadPath='./'):
-        # print('Getting attachments for mail {}'.format(mailId))
-
-        endpoint = self.Graph_API_Endpoint + f'/me/messages/{mailId}/attachments'
-        response = self.sendRequest(endpoint)
-
-        attachments = response['value']
-
-        if download:
-            for attachment in attachments:
-                with open(downloadPath + attachment['name'], 'wb') as f:
-                    f.write(base64.b64decode(attachment['contentBytes']))
-
-        return attachments
-
-    
-    # Searching for the appropriate attachment from the mail
-    def searchAndFind(self, df):
-        
-        data = df.to_dict('records')
-
-        for company in data:
-            searchText = format(int(company['searchItem']), ',.2f')
-            mails = self.searchMail(searchText, True)
-            print('Found {} mails for {}'.format(len(mails), searchText))
-            # print(len(mails), searchText)
-
-            for mail in tqdm(mails, desc='Searching for mails', unit='mails'):
-                self.getAttachments(mail['id'], download=True, downloadPath=Download_Cache)
-                
-                attachmentsList = os.listdir(Download_Cache)
-                attachmentsList.remove('.gitkeep')
-
-                # searching for the keyword inside the file
-                for attachment in attachmentsList:
-                    if searchFile(Download_Cache + attachment, searchText):
-                        # print( f'Found file with keyword({searchText}) inside attachment: {attachment}')
-                        shutil.copy(Download_Cache + attachment, Output + attachment)
-                
-                # deleting the downloaded cache
-                for attachment in attachmentsList:
-                    os.remove(Download_Cache + attachment)
-
-
-
-# Searching for the keyword inside the file
-def searchFile(filePath, searchText):
+# Searching for the keyword inside the PDF file
+def searchInsidePdf(filePath, searchText):
     pdfFile = PyPDF2.PdfFileReader(filePath)
     
     totalPages = pdfFile.getNumPages()
@@ -187,19 +35,55 @@ def searchFile(filePath, searchText):
     
     return found
 
-    
+# Searching for files with mail
+def main(df, graphApi):
+
+    data = df.to_dict('records')
+
+    for company in data:
+        searchText = format(int(company['searchItem']), ',.2f')
+
+        mails = graphApi.searchMail(searchText, True)
+        
+        print('Found {} mails for {}'.format(len(mails), searchText))
+        # print(len(mails), searchText)
+
+        for mail in tqdm(mails, desc='Searching the mails', unit='mails'):
+
+            attachments = graphApi.getAttachments(mail['id'], download=True, downloadPath=Download_Cache)
+            
+            attachmentsList = os.listdir(Download_Cache)
+            attachmentsList.remove('.gitkeep')
+
+            # searching for the keyword inside the file
+            for attachment in attachmentsList:
+                if searchInsidePdf(Download_Cache + attachment, searchText):
+                    # print( f'Found file with keyword({searchText}) inside attachment: {attachment}')
+                    shutil.copy(Download_Cache + attachment, Output + attachment)
+            
+            # deleting the downloaded cache
+            for attachment in attachmentsList:
+                os.remove(Download_Cache + attachment)
     
             
 
 if __name__ == '__main__':
 
     # Get the path to the file
-    filePath = sys.argv[1]
+    print(len(sys.argv), sys.argv)
+    if len(sys.argv) != 2:
+        print('Usage: python3 main.py \{fileName\}')
+        sys.exit(1)
+    else:
+        filePath = sys.argv[1]
+        if not os.path.exists(filePath):
+            print('File not found')
+            sys.exit(1)
 
     # Read the file
     df = pd.read_excel(filePath)
     # print(df.head())
 
-    mail = microsoftGraph('config.dev.cfg')
+    graphApi = microsoftGraph('config.dev.cfg')
 
-    mail.searchAndFind(df)
+    main(df, graphApi)
---
+++ microsoftgraph.py
@@ -0,0 +1,132 @@
+import os
+import sys
+import configparser
+import webbrowser
+import requests
+import base64
+
+
+# Microsoft Authentication Library
+import msal
+
+
+# Microsoft Graph API Class
+class microsoftGraph:
+
+    def __init__(self, cfgFile):
+        config = configparser.ConfigParser()
+        config.read([cfgFile])
+        azure_settings = config['azure']
+
+        # client and tenant, id and scopes
+        self.client_id = azure_settings['clientId']
+        self.tenant_id = azure_settings['tenantId']
+        self.scopes = azure_settings['graphUserScopes'].split(' ')
+        
+        self.Graph_API_Endpoint = 'https://graph.microsoft.com/v1.0'
+        
+        self.token = self.get_token()    
+
+    # return token for further api request
+    def get_token(self):
+
+        if os.path.exists('access_token.json'):
+            print('Found access_token.json')
+
+            access_token_cache = msal.SerializableTokenCache()
+
+            with open('access_token.json', 'r') as f:
+                access_token_cache.deserialize(f.read())
+
+            client = msal.PublicClientApplication(self.client_id, token_cache=access_token_cache)
+            
+            
+            accounts = client.get_accounts()[0]
+            print(accounts)
+
+            token = client.acquire_token_silent(self.scopes, account=accounts)
+
+        else:
+            print('No access_token.json found')
+            token = self.create_access_token()        
+        
+        return token
+
+    
+    # creating access token JSON file for the account
+    def create_access_token(self):
+        print("Creating access token")
+
+        access_token_cache = msal.SerializableTokenCache()
+
+        client = msal.PublicClientApplication(self.client_id, token_cache=access_token_cache)
+        flow = client.initiate_device_flow(scopes=self.scopes)
+
+        try:
+            verification_uri = flow['verification_uri']
+            user_code = flow['user_code']
+            print(flow['message'])
+            webbrowser.open(verification_uri)
+
+            # response access token and saving it
+            access_token = client.acquire_token_by_device_flow(flow)
+
+            with open('access_token.json', 'w') as f:
+                f.write(access_token_cache.serialize())
+        
+        except:
+            print('Error in creating access token')
+            print(flow)
+            sys.exit(1)
+        return access_token
+
+
+    # Send request to the Graph API
+    def sendRequest(self, endpoint):
+
+        headers = {
+            'Authorization': 'Bearer ' + self.token['access_token']
+        }
+
+        response = requests.get(endpoint, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print('Error in sending request')
+            # print(response.json())
+            sys.exit(1)
+
+    # Search Mail with the search text
+    def searchMail(self, searchText, hasAttachments=False):
+        # print('Searching for correct mails for {}'.format(searchText))
+
+        endpoint = self.Graph_API_Endpoint + f'/me/messages?$search="{searchText}"'
+
+        response = self.sendRequest(endpoint)
+        
+        mails = response['value']
+        # print(mails)
+
+        if hasAttachments:
+            temp = mails
+            mails = []
+            for mail in temp:
+                if mail['hasAttachments'] == True:
+                    mails.append(mail)
+        
+        return mails
+
+    def getAttachments(self, mailId, download=False, downloadPath='./'):
+        # print('Getting attachments for mail {}'.format(mailId))
+
+        endpoint = self.Graph_API_Endpoint + f'/me/messages/{mailId}/attachments'
+        response = self.sendRequest(endpoint)
+
+        attachments = response['value']
+
+        if download:
+            for attachment in attachments:
+                with open(downloadPath + attachment['name'], 'wb') as f:
+                    f.write(base64.b64decode(attachment['contentBytes']))
+
+        return attachments
